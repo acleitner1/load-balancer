3a9d29856f9d4423aaede5892d087bf4fd65c7a7
acleitner1 <78944987+acleitner1@users.noreply.github.com>
2024-06-19 09:23:21 -0400

    First pass at threading (step3) complete

--- lbpython.py
+++ lbpython.py
@@ -4,23 +4,44 @@
 import socket
 import sys
 import requests 
+import threading
+import time
 
 HOST, PORT = '', 80
+servers = []
+
+def health_check(interval, url): 
+   while(True): 
+      for i in range(len(servers)): 
+         health = requests.get(url + ":" + str(servers[i][0])) 
+         if (str(health) != "<Response [200]>"): 
+            print("health is: ")
+            print(health)
+            servers[i] = (servers[i][0], 0)
+         else: 
+            print("healthy")
+            servers[i] = (servers[i][0], 1)
+      time.sleep(interval)  
 
 lb = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 lb.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 lb.bind((HOST, PORT)) 
 lb.listen(1) 
-servers = []
 for i in range(len(sys.argv) - 2): 
    if (i != 0): 
-      servers.append(int(sys.argv[i]))
-interval = sys.argv[len(sys.argv) - 2]    
+      servers.append((int(sys.argv[i]), 1))
+interval = int(sys.argv[len(sys.argv) - 2])
 healthcode_url = sys.argv[len(sys.argv) - 1] 
+health_t = threading.Thread(target=health_check, args=(interval, healthcode_url), daemon = True)
 server_counter = 0
+health_t.start()
 while True: 
    if (server_counter >= len(servers)): 
       server_counter = 0
+   while (servers[server_counter][1] == 0): 
+      server_counter+=1 
+      if (server_counter > len(servers)): 
+         server_counter = 0   
    client_connection, client_address = lb.accept() 
    # print("client connection: " + client_connection)
    request_data = client_connection.recv(1024) 
@@ -30,12 +51,10 @@ while True:
    HTTP/1.1 200 OK 
 
    Message Recived: """ + request_data) 
-   BEHOST, BEPORT = '', servers[server_counter]
+   BEHOST, BEPORT = '', servers[server_counter][0]
    be = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
    be.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
    be.connect((BEHOST, BEPORT))
-   health = requests.get(healthcode_url + ":" + str(servers[server_counter])) 
-   print(health.status_code)
    be.sendall(request_data)
    be_data = be.recv(1024) 
    print("Recived: " + be_data.decode('utf-8') + "from be server: ")
@@ -43,3 +62,6 @@ while True:
    client_connection.close()
    be.close()
    server_counter+=1
+
+ 
+      
